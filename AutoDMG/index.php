0d135c586ab2039e2ba03dc370992188d4ba016a
Professor Coal <professorcoal@outlook.com>
2017-12-02 06:22:36 -0500

    https://www.skyjos.com/ftpmanager/help/help_main.php

---
+++ AutoDMG/index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<meta name="description" content="FTPManager,Skyjos,ftp,sftp,ftps,ios,app,iPhone,iPad"/>
+	<title>FTPManager - Skyjos</title>
+	<link rel="stylesheet" href="../../css/style.css" type="text/css" />
+	<script type="text/javascript" src="../../javascript/jquery.min.js"></script>
+</head>
+<body>
+
+<div id="logo_section">
+    <div style="width:50%;" class="fl">
+    <img height="60" align="middle" src="../app_logo.png"/>
+    <span style="display:inline-block; vertical-align:middle"><font size="6">FTPManager</font></span>
+    </div>
+</div>
+
+<div class="main" id="menu_section">
+	<ul>
+		<li id="home"><a href="../../index.html">Home</a></li>
+		<li id="features"><a href="../features/index.php">Features</a></li>
+		<li id="howto"><a href="../help/help_main.php">HowTo</a></li>
+		<li id="support"><a href="#">Support</a></li>
+	</ul>
+</div>
+
+<div id="main_section">
+	<div id="sub_contents_section">
+	    <div id="sub_contents_top"></div>
+		<div id="sub_contents_mid02" style="padding-left:35px;">
+			<h3>How to contact us?</h3>
+Please feel free to send us email for any questions, suggestions, supports. 
+<a href="mailto:support@skyjos.com">support@skyjos.com</a>
+			<br/><br/>
+			<h3>How to get development status?</h3>
+			Follow us on twitter: <a href="http://www.twitter.com/SkyjosApps">@SkyjosApps</a><br/><br/>
+		</div>
+		<div id="sub_contents_btm03"></div>
+	</div>
+</div>
+
+<div id="btm_section">
+    Copyright &copy; 2016 by Skyjos Co., Ltd. All rights reserved.
+</div>
+</body>
+</html>
